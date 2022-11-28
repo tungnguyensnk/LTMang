3a44a820d7b85bb4783d5b2e85dbbebec2918919
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2022-11-28 10:34:14 +0700

    test

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+int main()
+{
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    SOCKADDR_IN saddr, ackaddr;
+    ackaddr.sin_family = AF_INET;
+    ackaddr.sin_port = htons(6000);
+    ackaddr.sin_addr.s_addr = 0;
+    bind(s, (SOCKADDR*)&ackaddr, sizeof(ackaddr));
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(5000);
+    saddr.sin_addr.s_addr = inet_addr("172.30.236.220");
+    while (1)
+    {
+        char buffer[1024] = { 0 };
+        fgets(buffer, sizeof(buffer), stdin);
+        int sent = sendto(s, buffer, strlen(buffer), 0, (SOCKADDR*)&saddr, sizeof(saddr));
+        char ack[1024] = { 0 };
+        recvfrom(s, ack, sizeof(ack), 0, NULL, NULL);
+        printf("ACK: %s\n", ack);
+    }
+    close(s);
 }
